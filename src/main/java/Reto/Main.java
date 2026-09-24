2bff5c8b708a0cc63bd9561193320683aa932f16
Ander699 <buitragoguerreroanderson@gmail.com>
2026-09-24 12:58:23 -0500

    Parte del reto

--- src/main/java/Reto/Main.java
+++ src/main/java/Reto/Main.java
@@ -1,7 +1,22 @@
 package Reto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args) {
-        
+        List<Producto> productos = new ArrayList<>();
+
+        productos.add(new Producto("Pala", "Herramienta", 100.2));
+        productos.add(new Producto("Pico", "Herramienta", 143535.2));
+        productos.add(new Producto("Azada", "Herramienta", 1455.2));
+        productos.add(new Producto("Cemento", "Material", 453.2));
+        productos.add(new Producto("Ladrillos", "Material", 10345.2));
+        productos.add(new Producto("Arena", "Material", 14.6));
+
+        productos.sort((a, b) -> a.categoria.compareTo(b.categoria));
+        for (Producto producto : productos) {
+            System.out.println(producto);
+        }
     }
 }
--- src/main/java/Reto/Producto.java
+++ src/main/java/Reto/Producto.java
@@ -1,4 +1,23 @@
 package Reto;
 
-public class Productos {
+public class Producto {
+
+    public String nombre_Producto   ;
+    public String categoria   ;
+    public double precio   ;
+
+    public Producto(String nombre_Producto, String categoria, double precio) {
+        this.nombre_Producto = nombre_Producto;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" +
+                "nombre_Producto='" + nombre_Producto + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", precio='" + precio + '\'' +
+                '}';
+    }
 }
